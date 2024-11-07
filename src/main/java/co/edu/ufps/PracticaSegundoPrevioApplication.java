a45c3c1e94664342a5b5525e24eb7250816cdc01
FelipeB <andresfelipeba@ufps.edu.co>
2024-11-07 15:29:34 -0500

    creo el proyecto :D

---
+++ src/main/java/co/edu/ufps/PracticaSegundoPrevioApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSegundoPrevioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSegundoPrevioApplication.class, args);
+	}
+
+}
